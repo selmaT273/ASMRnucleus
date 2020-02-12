99f24b345c99de01a9e4dc263e334d78a3741bb0
Stacey Teltser <stacey.teltser@gmail.com>
2020-02-12 17:53:16 -0600

    added javascript to display prompt alert and todays date

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log('hopefully this works');
+
+
+var preference = prompt('Which kind of ASMR would you like to experience? Visual or Audio?');
+if(preference == 'Visual'){
+    alert('see link at the bottom of the page to be redirected to Visual ASMR');
+} else if(preference == Audio){
+    alert('you came to the right place! enjoy!');
+}
