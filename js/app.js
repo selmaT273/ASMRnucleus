31815bc9f9e9adcc34a61ec173b00f617f028644
Stacey Teltser <stacey.teltser@gmail.com>
2020-02-13 11:41:50 -0600

    refactored javascript by moving it to the js file and then invoking it in the html page; removed case sensitivity via toLowercase; added text to be prefilled in the prompt box; added another alert option if user does not input visual or audio

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 console.log('hopefully this works');
 
 
-var preference = prompt('Which kind of ASMR would you like to experience? Visual or Audio?');
-if(preference == 'Visual'){
+var preference = prompt('Which kind of ASMR would you like to experience? Visual or Audio?', 'I know its hard to choose').toLowerCase();
+if(preference == 'visual'){
     alert('see link at the bottom of the page to be redirected to Visual ASMR');
-} else if(preference == Audio){
+} else if(preference == 'audio'){
     alert('you came to the right place! enjoy!');
+} else {
+    alert('your response is not valid...try again')
+}
+
+
+function printToday(){
+    var today = new Date();
+    document.write(today.toDateString())
 }
